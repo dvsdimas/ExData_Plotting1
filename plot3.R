3885351f0141cb051c950dec0ca64a3099a2fe18
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2016-11-27 14:50:00 +0300

    finished plot 3

---
+++ plot3.R
@@ -0,0 +1,14 @@
+if(!exists("get_tidy_data", mode = "function")) source("get_tidy_data.R")
+
+D <- get_filtered_data()
+
+png("plot3.png", width = 480, height = 480)
+
+plot(D$DateTime, D$SubMetering1, ylab = "Energy sub metering", xlab = "", type = "l")
+
+lines(D$DateTime, D$SubMetering2, type = "l", col = "red")
+lines(D$DateTime, D$SubMetering3, type = "l", col = "blue")
+
+legend("topright", c("SubMetering1", "SubMetering2", "SubMetering3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+dev.off()
